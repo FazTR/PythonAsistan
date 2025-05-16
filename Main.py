2e61c77754ed1641e398a6a3892aec097246c7b7
FazTR <53142913+FazTR@users.noreply.github.com>
2025-05-16 17:10:07 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,17 @@
+import whisper
+import speech_recognition as sr
+
+r = sr.Recognizer()
+model = whisper.load_model("medium")
+
+with sr.Microphone() as source:
+    print("Hazır, konuşabilirsiniz...")
+    audio = r.listen(source)
+
+# Audio verisini dosya olarak kaydet
+with open("gecici_kayit.wav", "wb") as f:
+    f.write(audio.get_wav_data())
+
+# Whisper ile çözümle
+result = model.transcribe("gecici_kayit.wav", language="tr")
+print("Sonuç:", result["text"])
