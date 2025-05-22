fbfacdaa1b509bf024ae00a29021496beab344b9
FazTR <53142913+FazTR@users.noreply.github.com>
2025-05-22 03:58:22 +0300

    Connected to Main

--- predict_intent.py
+++ predict_intent.py
@@ -1,19 +1,16 @@
-# predict.py
-
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import torch.nn.functional as F
 import json
 
-# 1. Label listesini yükle
+# Etiket listesini yükle
 with open("label_list.json", "r", encoding="utf-8") as f:
     label_list = json.load(f)
 
-# 2. Model ve tokenizer'ı yükle
+# Model ve tokenizer
 tokenizer = AutoTokenizer.from_pretrained("./results")
 model = AutoModelForSequenceClassification.from_pretrained("./results")
 
-# 3. Tahmin fonksiyonu
 def predict_intent(text):
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding='max_length', max_length=64)
     outputs = model(**inputs)
@@ -21,9 +18,3 @@ def predict_intent(text):
     probs = F.softmax(logits, dim=1)
     predicted_class_id = torch.argmax(probs).item()
     return label_list[predicted_class_id]
-
-# 4. Test
-if __name__ == "__main__":
-    test_text = "bugün hava çok kötü hava tahminleri nasıl"
-    prediction = predict_intent(test_text)
-    print(f"'{test_text}' -> Tahmin Edilen Niyet: {prediction}")
